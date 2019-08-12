a0108bdd0752c5e5fa80a1fd294b88b089432afe
Jaime Vega <jhuesos@gmail.com>
2019-08-12 09:53:36 +0100

    build: add cache busting to output files

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,13 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, './src/index.js'),
+
   output: {
     path: path.join(__dirname, './dist'),
-    filename: 'index.min.js',
+    filename: '[name].[contenthash].min.js',
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, './src/index.tpl.ejs'),
     }),
   ],
+
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+  },
 };
