6104f6c2a754da948977da5df03bd3521b030be3
Jaime Vega <jhuesos@gmail.com>
2019-08-14 13:35:35 +0100

    build: add css related loaders and optimizations

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.join(__dirname, './src/index.js'),
@@ -9,14 +10,41 @@ module.exports = {
     filename: '[name].[contenthash].min.js',
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, './src/index.tpl.ejs'),
-    }),
-  ],
-
   optimization: {
     moduleIds: 'hashed',
     runtimeChunk: 'single',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.tpl.ejs'),
+    }),
+  ],
 };
